1725fa89e9bad484a5542662d83dbd73d53d5594
Hussein <hmtirawi@gmail.com>
2018-04-15 01:13:54 +0300

    parser and post method

--- server/index.js
+++ server/index.js
@@ -5,19 +5,29 @@ var items = require('../database-mongo');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/../react-client/dist'));
 
+app.post('/items', function(req,res){	
+	console.log(req.body)
+});
+
+
+
 app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
+	items.selectAll(function(err, data) {
+		if(err) {
+			res.sendStatus(500);
+		} else {
+			res.json(data);
+		}
+	});
 });
 
 app.listen(3000, function() {
-  console.log('listening on port 3000!');
+	console.log('listening on port 3000!');
 });
 
