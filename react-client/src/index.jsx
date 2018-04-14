3f170ff8ecda1f6549e702dae492c7404a450469
Hussein <hmtirawi@gmail.com>
2018-04-15 01:15:53 +0300

    edit ajax methods

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import List from './components/List.jsx';
+import ListItem from './components/ListItem.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,9 +12,22 @@ class App extends React.Component {
     }
   }
 
-  componentDidMount() {
+  componentDidMount(value) {
     $.ajax({
       url: '/items', 
+      type: 'POST',
+      data : {movieName : value},
+      success: (data) => {
+        console.log(data)
+      },
+      error: (err) => {
+        console.log('err', err);
+      }
+    });
+
+    $.ajax({
+      url: '/items', 
+      type: 'GET',
       success: (data) => {
         this.setState({
           items: data
@@ -29,7 +43,8 @@ class App extends React.Component {
     return (<div>
       <h1>Item List</h1>
       <List items={this.state.items}/>
-    </div>)
+      <ListItem onSearch={this.componentDidMount.bind(this)}/>
+      </div>)
   }
 }
 
