7d872ddf7051b7d247035f808af7824594d16ef3
Hussein <hmtirawi@gmail.com>
2018-04-15 01:12:31 +0300

    add page functions

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
-const ListItem = (props) => (
-  <div>
-    { props.item.description }
-  </div>
-)
+
+class ListItem extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {searchedVal : ""};
+		this.onClick = this.onClick.bind(this)
+		this.onChange = this.onChange.bind(this)
+	}
+
+	onClick(){
+		this.props.onSearch(this.state.searchedVal);
+	}
+
+	onChange(e){
+		this.setState({
+			searchedVal:e.target.value
+		})
+
+	}
+
+	render(){
+		return(
+			<div>
+			<input value={this.state.searchedVal} onChange={this.onChange}></input> 
+			<button onClick={this.onClick}>Search</button>
+			</div>
+			)
+
+	}
+	
+}
 
 export default ListItem;
