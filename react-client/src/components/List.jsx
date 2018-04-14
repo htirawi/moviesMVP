9d1a2895ac10223e0a0d0c4efe5f60e63c4efa69
Hussein <hmtirawi@gmail.com>
2018-04-15 00:33:14 +0300

    return a movie name

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ListItem from './ListItem.jsx';
 
 const List = (props) => (
-  <div>
-    <h4> List Component </h4>
-    There are { props.items.length } items.
-    { props.items.map(item => <ListItem item={item}/>)}
-  </div>
-)
+	<div>
+	<h4> List Component </h4>
+	There are { props.items.length } items.
+	{props.items.map(function(item){
+		return <h1> {item.movieName} </h1>
+	})}
+	</div>
+	)
 
 export default List;
+
+    // { props.items.map(item => <ListItem item={item}/>)}
